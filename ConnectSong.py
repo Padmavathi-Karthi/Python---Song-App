2ba93284264290bfb33356311d9c68f87712b3bc
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-02-22 21:29:49 +0000

    database changed

--- ConnectSong.py
+++ ConnectSong.py
@@ -1,6 +1,6 @@
 import sqlite3 as sql
 
 # connect(filepath to database)
-connection = sql.connect(r"C:\Users\padhu\Desktop\JustIT\Python\Final Song Database Project\Music1.db")
+connection = sql.connect(r"C:\Users\padhu\Desktop\GitHub\Python---Song-App\Music.db")
 
 cursor = connection.cursor()
