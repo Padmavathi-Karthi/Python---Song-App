8d078a270461e404c93edfb848960c0ce5825c54
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-02-22 21:20:40 +0000

    table created

---
+++ CreateTables.py
@@ -0,0 +1,22 @@
+from ConnectSong import *
+
+"""
+MULTI - LINE STRING
+
+"""
+
+cursor.execute(
+
+"""
+CREATE TABLE "Music1"(
+    "SongID"    INTEGER NOT NULL UNIQUE,
+    "Title"     TEXT,
+    "Year"      INTEGER,
+    "Artist"    TEXT,
+    "Genre"     TEXT,
+    PRIMARY KEY("SongID" AUTOINCREMENT)
+
+)
+
+"""
+)
